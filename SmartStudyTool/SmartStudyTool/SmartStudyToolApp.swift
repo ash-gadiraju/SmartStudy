2d17caad90fcf17ef600334044a51d1df8cc4308
Ash G <ashgadiaju@gmail.com>
2026-06-02 17:34:22 -0500

    Built app template in folder

---
+++ SmartStudyTool/SmartStudyTool/SmartStudyToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyToolApp.swift
+//  SmartStudyTool
+//
+//  Created by Ashritha Gadiraju on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartStudyToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
