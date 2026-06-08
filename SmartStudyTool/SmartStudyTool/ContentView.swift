1d5339fe3efb0ab47b3eea17dfe4aa48912c1cad
Ash G <ashgadiaju@gmail.com>
2026-06-07 20:01:21 -0500

    Adding BGE Embedding package depency
    I'm just gonna guestimate this whole project ig...

--- SmartStudyTool/SmartStudyTool/ContentView.swift
+++ SmartStudyTool/SmartStudyTool/ContentView.swift
@@ -1,11 +1,23 @@
 //
 //  ContentView.swift
 //  SmartStudyTool
-//
+//  Embedder: BGE
+//  Embedder documentation: https://bge-model.com/Introduction/installation.html
 //  Created by Ashritha Gadiraju on 6/2/26.
 //
 
 import SwiftUI
+
+
+// Questionss pipeline:
+// user enters prompt for quiz --> app sends prompt to Chroma thru embedder
+// Chroma gets relevant data --> sends to app
+// app builds prompt for Ollama --> Ollama generates response for app
+// App gives user questions
+// Answers pipeline:
+// user gives answers to app --> Apps sends question, correct solution, and user answer to Ollama
+// Ollama grades --> Sends evaluation to app
+// App sends evaluation and correct answers to user
 
 struct ContentView: View {
     var body: some View {
