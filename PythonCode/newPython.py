6fb25ce205ab90497e83678259b9377e26fd2eb1
Ash G <ashgadiaju@gmail.com>
2026-06-22 17:10:06 -0500

    btw deleted numdoc bc I need only relevant stuff not all docs

--- PythonCode/newPython.py
+++ PythonCode/newPython.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f54e7926a388379a4b98ff9068add9750d7133aa6da6990b5a979b7dd9fdd75
-size 4011
+oid sha256:444001bb8cfb8008edff0046af8e60210eb3a54a21be49e2a4f0a0b1100b3d86
+size 3880
