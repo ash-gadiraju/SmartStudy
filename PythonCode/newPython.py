0582f6d74533181ae6670edc8fa62f10f06aadba
Ash G <ashgadiaju@gmail.com>
2026-06-23 09:51:05 -0500

    Updated the helper fxns and global vars

--- PythonCode/newPython.py
+++ PythonCode/newPython.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db1939082649220ded5077d9bf6ab2cf5b4f8c86628fa295496048d323692a91
-size 3236
+oid sha256:bd79afeabd8030ad81efcf0c9faa1b24285eee14ddab5e4e6bfcb22949e02d52
+size 3314
